5473e23409474579609dba5da3eb05cc499002a6
mr.lin <278971585@qq.com>
2018-10-02 21:01:04 +0800

    增加ContentController类，控制内容的读取及显示 添加txt文件

--- Paging/app/src/main/java/com/ljf/paging/MainActivity.java
+++ Paging/app/src/main/java/com/ljf/paging/MainActivity.java
@@ -1,14 +1,24 @@
 package com.ljf.paging;
 
 import android.app.Activity;
-import android.graphics.Point;
 import android.os.Bundle;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class MainActivity extends Activity {
+
+    private BookPageView pageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pageView = findViewById(R.id.pageView);
+        ContentController contentController = new ContentController(this, pageView);
     }
+
+
 }
