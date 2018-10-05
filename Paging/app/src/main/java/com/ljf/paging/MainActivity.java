2cac1da938c3c4fa92c0940df2ea89b73e5cfc75
mr.lin <278971585@qq.com>
2018-10-05 10:27:10 +0800

    全屏

--- Paging/app/src/main/java/com/ljf/paging/MainActivity.java
+++ Paging/app/src/main/java/com/ljf/paging/MainActivity.java
@@ -3,20 +3,12 @@ package com.ljf.paging;
 import android.app.Activity;
 import android.os.Bundle;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
 public class MainActivity extends Activity {
-
-    private BookPageView pageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 
 }
